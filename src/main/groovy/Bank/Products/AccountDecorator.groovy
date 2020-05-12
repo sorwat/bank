6cc0d65d5b34b645c9786246f3b62fd531486246
Ananas <ana.rei.014@gmail.com>
2020-05-12 22:26:28 +0200

    add decorator class

--- src/main/groovy/Bank/Products/AccountDecorator.groovy
+++ src/main/groovy/Bank/Products/AccountDecorator.groovy
@@ -1,4 +1,13 @@
 package Bank.Products
 
 class AccountDecorator {
+    private @Delegate
+    Account account
+    private overdraft_balance
+
+    def overdraft(debit, BigDecimal amount) {
+        if (debit < 0 && balance == 0) {
+            overdraft_balance -= amount
+        }
+    }
 }
