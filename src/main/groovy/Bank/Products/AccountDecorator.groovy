72b4bd4e3bfde62f1e0ea5f8bb3d45895ab3299d
Ananas <ana.rei.014@gmail.com>
2020-05-12 23:03:34 +0200

    add test for class

--- src/main/groovy/Bank/Products/AccountDecorator.groovy
+++ src/main/groovy/Bank/Products/AccountDecorator.groovy
@@ -4,9 +4,14 @@ class AccountDecorator {
     private @Delegate
     Account account
     private overdraft_balance
+    BigDecimal debit
 
-    def overdraft(debit, BigDecimal amount) {
-        if (debit < 0 && balance == 0) {
+    void updateDebitCapability(BigDecimal debitCapability) {
+        debit -= debitCapability
+    }
+
+    def overdraft(BigDecimal amount) {
+        if ((debit < 0 && balance == 0.00) || ((balance - amount) < 0 && debit < 0)) {
             overdraft_balance -= amount
         }
     }
---
+++ src/test/groovy/Products/AccountDecoratorTest.groovy
@@ -0,0 +1,21 @@
+package Products
+
+import Bank.Customer
+import Bank.Products.Account
+import Bank.Products.AccountDecorator
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class AccountDecoratorTest extends Specification{
+    @Unroll
+    def 'test Account Decorator should update debit Capability'() {
+        when:
+        Customer customer = new Customer()
+        Account account = new Account(0, customer)
+        AccountDecorator accountDecorator = new AccountDecorator(account: account)
+        accountDecorator.updateDebitCapability(111);
+
+        then:
+        accountDecorator.debit == -111.00
+    }
+}
